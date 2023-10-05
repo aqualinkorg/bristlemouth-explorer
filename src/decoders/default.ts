18cdd1dec86ab0b4e5462b801ecc82e7eedc8873
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-10-05 15:30:29 +0300

    Add comments

--- src/decoders/default.ts
+++ src/decoders/default.ts
@@ -1,3 +1,11 @@
+/**
+ * This is a sample decoder configuration, as per the example provided here: 
+ * https://bristlemouth.notion.site/Bristlemouth-Dev-Kit-Guide-5-Developing-a-Simple-Application-a69c351dadba4772926ada25a08a43fc
+ * A decoder comprises a list of sensors. Each sensor is described by a struct, 
+ * which contains information about the datatype and the name of the encoded values.
+ * The "display" attribute is used to select the values that will be displayed on the table in this application.
+ */
+
 import { Decoder, SensorStruct } from 'src/helpers/types';
 
 const defaultSensorStruct: SensorStruct = [
--- src/helpers/decoder.ts
+++ src/helpers/decoder.ts
@@ -9,6 +9,12 @@ interface HexToStructProps {
   offset: number;
 }
 
+/**
+ * This function converts hexadecimal data to a structured format.
+ * It takes a buffer of byte data, a description of the sensor structure, and an offset as input.
+ * The function iterates over the byte data according to the sensor structure and converts the data to a structured format.
+ * The offset is used to keep track of the position in the byte data.
+ */
 function hexToStruct({
   byteData,
   structDescription,
@@ -45,6 +51,11 @@ interface DecodeProps {
   hexData: string;
 }
 
+/**
+ * This function decodes the hex data using the provided decoder configuration.
+ * It iterates over each struct in the decoder configuration, and for each struct, it decodes the corresponding part of the hex data.
+ * The decoded data is then stored in the result object, with the struct name as the key.
+ */
 export function decode({ decoderConfig, hexData }: DecodeProps): DecoderOutput {
   const byteData = Buffer.from(hexData.replace(/\s+/g, ''), 'hex');
   const result: DecoderOutput = {};
